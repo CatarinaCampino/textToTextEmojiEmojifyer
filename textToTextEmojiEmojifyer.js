ec17b4f6d57de85263eaf64fc70070272f39affc
Catarina Campino <CatarinaCampino@Catarinas-MacBook-Pro-2.local>
2017-10-24 22:18:54 +0100

    Add beautified and indented file

--- textToTextEmojiEmojifyer.js
+++ textToTextEmojiEmojifyer.js
@@ -1,12 +1,12 @@
 var text;
 var textTurnedTextEmoji;
 
-function textToTextEmoji (text) {
-console.log(":" + text + ":");
-textTurnedTextEmoji = ":" + text + ":";
+function textToTextEmoji(text) {
+  console.log(": " + text + " :");
+  textTurnedTextEmoji = ": " + text + " :";
 }
 
-text="Estou fodido";
+text = "Estou fodido";
 textToTextEmoji(text);
 
 console.log(textTurnedTextEmoji);
